72d343130d99e2a90bffe30150565f0c2583975b
ronitnag04 <ronitnag04@gmail.com>
2023-08-10 23:53:08 -0700

    Unstable: Developing Flags control

--- Naga V0/Control Microcode EEPROM Programmer/Control Microcode EEPROM Programmer.ino
+++ Naga V0/Control Microcode EEPROM Programmer/Control Microcode EEPROM Programmer.ino
@@ -20,7 +20,7 @@
 #define EO  0b0000000000000011
 #define AO  0b0000000000000100
 #define CO  0b0000000000000101
-#define UO0 0b0000000000000110
+#define UNO 0b0000000000000110
 
 #define II  0b0000000000001000
 #define RI  0b0000000000010000
@@ -34,7 +34,7 @@
 #define CI  0b0001000000000000
 #define FI  0b0010000000000000
 #define MCR 0b0100000000000000
-#define UC0 0b1000000000000000
+#define UNC 0b1000000000000000
 
 void setAddress(int address, bool outputEnable) {
   shiftOut(SHIFT_DATA, SHIFT_CLK, MSBFIRST, address >> 8 | (outputEnable ? 0x00 : 0x80));
@@ -107,30 +107,51 @@ void eraseEEPROM() {
   Serial.println(" done!");
 }
 
-bool left_chip;
-
-uint16_t data[] = {
-  CO|MI, RO|II|CE, MCR, DFT, DFT, DFT, DFT, DFT,           // NOP 0000
-  CO|MI, RO|II|CE, IO|MI, RO|AI, MCR, DFT, DFT, DFT,       // LDA 0001
-  CO|MI, RO|II|CE, IO|MI, RO|BI, MCR, DFT, DFT, DFT,       // LDB 0010
-  CO|MI, RO|II|CE, EO|AI, MCR, DFT, DFT, DFT, DFT,         // ADD 0011
-  CO|MI, RO|II|CE, SU|EO|AI, MCR, DFT, DFT, DFT, DFT,      // SUB 0100
-  CO|MI, RO|II|CE, IO|MI, AO|RI, MCR, DFT, DFT, DFT,       // STA 0101
-  CO|MI, RO|II|CE, IO|AI, MCR, DFT, DFT, DFT, DFT,         // LDI 0110
-  CO|MI, RO|II|CE, IO|CI, MCR, DFT, DFT, DFT, DFT,         // JMP 0111
-  CO|MI, RO|II|CE, MCR, DFT, DFT, DFT, DFT, DFT,           //     1000
-  CO|MI, RO|II|CE, MCR, DFT, DFT, DFT, DFT, DFT,           //     1001
-  CO|MI, RO|II|CE, MCR, DFT, DFT, DFT, DFT, DFT,           //     1010
-  CO|MI, RO|II|CE, MCR, DFT, DFT, DFT, DFT, DFT,           //     1011
-  CO|MI, RO|II|CE, MCR, DFT, DFT, DFT, DFT, DFT,           //     1100
-  CO|MI, RO|II|CE, MCR, DFT, DFT, DFT, DFT, DFT,           //     1101
-  CO|MI, RO|II|CE, IO|MI, RO|OI, MCR, DFT, DFT, DFT,       // OUT 1110
-  CO|MI, RO|II|CE, HLT|DFT, MCR, DFT, DFT, DFT, DFT,           // HLT 1111
+const PROGMEM uint16_t code[32][8] = {
+  { CO|MI, RO|II|CE, MCR, DFT, DFT, DFT, DFT, DFT,         },  // NOP 00000
+  { CO|MI, RO|II|CE, IO|MI, RO|AI, MCR, DFT, DFT, DFT,     },  // LDA 00001
+  { CO|MI, RO|II|CE, IO|MI, RO|BI, MCR, DFT, DFT, DFT,     },  // LDB 00010
+  { CO|MI, RO|II|CE, EO|AI|FI, MCR, DFT, DFT, DFT, DFT,    },  // ADD 00011
+  { CO|MI, RO|II|CE, SU|EO|AI|FI, MCR, DFT, DFT, DFT, DFT, },  // SUB 00100
+  { CO|MI, RO|II|CE, IO|MI, AO|RI, MCR, DFT, DFT, DFT,     },  // STA 00101
+  { CO|MI, RO|II|CE, IO|AI, MCR, DFT, DFT, DFT, DFT,       },  // LDI 00110
+  { CO|MI, RO|II|CE, IO|CI, MCR, DFT, DFT, DFT, DFT,       },  // JMP 00111
+  { CO|MI, RO|II|CE, MCR, DFT, DFT, DFT, DFT, DFT,         },  // JZ  01000
+  { CO|MI, RO|II|CE, MCR, DFT, DFT, DFT, DFT, DFT,         },  // JN  01001
+  { CO|MI, RO|II|CE, MCR, DFT, DFT, DFT, DFT, DFT,         },  // JC  01010
+  { CO|MI, RO|II|CE, MCR, DFT, DFT, DFT, DFT, DFT,         },  //     01011
+  { CO|MI, RO|II|CE, MCR, DFT, DFT, DFT, DFT, DFT,         },  //     01100
+  { CO|MI, RO|II|CE, MCR, DFT, DFT, DFT, DFT, DFT,         },  //     01101
+  { CO|MI, RO|II|CE, IO|MI, RO|OI, MCR, DFT, DFT, DFT,     },  // OUT 01110
+  { CO|MI, RO|II|CE, HLT|DFT, MCR, DFT, DFT, DFT, DFT,     },  // HLT 01111
+  { CO|MI, RO|II|CE, MCR, DFT, DFT, DFT, DFT, DFT,         },  //     10000
+  { CO|MI, RO|II|CE, MCR, DFT, DFT, DFT, DFT, DFT,         },  //     10001
+  { CO|MI, RO|II|CE, MCR, DFT, DFT, DFT, DFT, DFT,         },  //     10010
+  { CO|MI, RO|II|CE, MCR, DFT, DFT, DFT, DFT, DFT,         },  //     10011
+  { CO|MI, RO|II|CE, MCR, DFT, DFT, DFT, DFT, DFT,         },  //     10100
+  { CO|MI, RO|II|CE, MCR, DFT, DFT, DFT, DFT, DFT,         },  //     10101
+  { CO|MI, RO|II|CE, MCR, DFT, DFT, DFT, DFT, DFT,         },  //     10110
+  { CO|MI, RO|II|CE, MCR, DFT, DFT, DFT, DFT, DFT,         },  //     10111
+  { CO|MI, RO|II|CE, MCR, DFT, DFT, DFT, DFT, DFT,         },  //     11000
+  { CO|MI, RO|II|CE, MCR, DFT, DFT, DFT, DFT, DFT,         },  //     11001
+  { CO|MI, RO|II|CE, MCR, DFT, DFT, DFT, DFT, DFT,         },  //     11010
+  { CO|MI, RO|II|CE, MCR, DFT, DFT, DFT, DFT, DFT,         },  //     11011
+  { CO|MI, RO|II|CE, MCR, DFT, DFT, DFT, DFT, DFT,         },  //     11100
+  { CO|MI, RO|II|CE, MCR, DFT, DFT, DFT, DFT, DFT,         },  //     11101
+  { CO|MI, RO|II|CE, MCR, DFT, DFT, DFT, DFT, DFT,         },  //     11110
+  { CO|MI, RO|II|CE, MCR, DFT, DFT, DFT, DFT, DFT,         },  //     11111
 };
 
-
-
+#define FLAGS_Z0N0C0 0
+#define FLAGS_Z0N0C1 1
+#define FLAGS_Z0N1C0 2
+#define FLAGS_Z0N1C1 3
+#define FLAGS_Z1N0C0 4
+#define FLAGS_Z1N0C1 5
+#define FLAGS_Z1N1C0 6
+#define FLAGS_Z1N1C1 7
  
+bool left_chip;
 void setup() {
   pinMode(SHIFT_DATA, OUTPUT);
   pinMode(SHIFT_CLK, OUTPUT);
@@ -145,27 +166,67 @@ void setup() {
   Serial.begin(57600);
   
   // eraseEEPROM();
-
   if (left_chip) {
     Serial.print("Programming Left EEPROM");
   } else {
     Serial.print("Programming Right EEPROM");
   }
-  for (int address = 0; address < sizeof(data)/sizeof(data[0]); address += 1) {
-      if (left_chip) {
-        writeEEPROM(address, data[address] >> 8);
-      } else {
-        writeEEPROM(address, data[address]);
+  
+  for (int address = 0; address < 2048; address += 1) {
+    // [10][9][8][7][6][5][4][3][2][1][0]
+    // |  flags  |    opcode    |  step |
+    int flags =  (address & 0b11100000000) >> 8;
+    int opcode = (address & 0b00011111000) >> 3;
+    int step =   (address & 0b00000000111);
+      
+    uint16_t controlWord = code[opcode][step];
+    if (opcode == 0b01000) {
+      if (flags == FLAGS_Z1N0C0 ||
+          flags == FLAGS_Z1N0C1 ||
+          flags == FLAGS_Z1N1C0 ||
+          flags == FLAGS_Z1N1C1) {
+            if (step == 2) {
+              controlWord = IO|CI;
+            } else if (step == 3) {
+              controlWord = MCR;
+            }
       }
-
+    } else if (opcode == 0b01001) {
+      if (flags == FLAGS_Z0N1C0 ||
+          flags == FLAGS_Z0N1C1 ||
+          flags == FLAGS_Z1N1C0 ||
+          flags == FLAGS_Z1N1C1) {
+            if (step == 2) {
+              controlWord = IO|CI;
+            } else if (step == 3) {
+              controlWord = MCR;
+            }
+      }
+    } else if (opcode == 0b01010) {
+      if (flags == FLAGS_Z0N0C1 ||
+          flags == FLAGS_Z0N1C1 ||
+          flags == FLAGS_Z1N0C1 ||
+          flags == FLAGS_Z1N1C1) {
+            if (step == 2) {
+              controlWord = IO|CI;
+            } else if (step == 3) {
+              controlWord = MCR;
+            }
+      }
+    }
+    if (left_chip) {
+      writeEEPROM(address, controlWord >> 8);
+    } else {
+      writeEEPROM(address, controlWord);
+    }
+    
     if (address % 64 == 0) {
       Serial.print(".");
     }
   }
-  Serial.println();
 
+  Serial.println(); 
   printContents();
-  
 }
 
 void loop() { 
